946acc8ddd3d27e5dd91dbf4cee3329056b96325
averyanov alexey <averyanovalexey@gmail.com>
2022-08-06 13:19:24 +0600

    update to airflow 2.1.4

--- script/init.sh
+++ script/init.sh
@@ -2,8 +2,14 @@
 
 # Создание БД
 sleep 10
-airflow upgradedb
+# airflow upgradedb
+airflow db init
 sleep 10
+
+# Создаем админа
+sleep 10
+airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.org -p admin
 
 # Запуск шедулера и вебсервера
 airflow scheduler & airflow webserver
+
